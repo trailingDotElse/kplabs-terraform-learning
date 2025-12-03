fdcf9dda1f294a6e7ac5c74fb7fa347e07b0b9c3
William Leonard Johns <133729936+trailingDotElse@users.noreply.github.com>
2025-12-03 08:56:45 -0500

    Update EC2 resource block and provider syntax

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.22.1"
-    }
-  }
-}
 
 
-resource "ec2-instance" "myec2" {
+
+resource "aws_instance" "myec2" {
   region = "us-east-1"
   ami = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
